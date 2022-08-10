9435f78c087fada5c70e9948016bdfb32798ea7d
siddBhandari <siddharthrbhandari2002@gmail.com>
2022-08-10 21:46:38 +0530

    1832

---
+++ 1832_check_if_the_sentence_is_panagram.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        int arr[26] = {0};
+        for (int i=0; i<sentence.length(); i++){
+            arr[sentence[i] - 'a']++;
+        }
+        for(int i=0; i<26; i++){
+            if(arr[i] < 1){
+                return false;
+            }
+        }
+        return true;
+    }
+};
